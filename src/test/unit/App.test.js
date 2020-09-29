bf98d93825bed8bee549a10fc97601ccf71d7710
Takahiro Hasegawa <23368890+Takahero@users.noreply.github.com>
2020-09-30 00:10:22 +0300

    Add smoke test

--- src/test/unit/App.test.js
+++ src/test/unit/App.test.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from '../.././App';
 
-test('renders learn react link', () => {
-    const { getByText } = render( < App / > );
-    const linkElement = getByText(/learn react/i);
-    expect(linkElement).toBeInTheDocument();
-});
+describe('Smoke test', () => {
+    it('renders App', () => {
+        const { getByTestId } = render(<App/>)
+
+        expect(getByTestId('App')).toBeTruthy()
+    })
+})

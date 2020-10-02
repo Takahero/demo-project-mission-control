208048219e98e63604bfe7960962c0b58fb35be2
Takahiro Hasegawa <23368890+Takahero@users.noreply.github.com>
2020-10-02 18:27:44 +0300

    Create project links

--- src/components/organisms/ProjectList.tsx
+++ src/components/organisms/ProjectList.tsx
@@ -4,26 +4,36 @@ import ProjectCard from '../molecules/ProjectCard'
 import { mockProjects } from '../../utils/mockProjectsData'
 import { shortFullName } from '../../utils/name';
 import { projectDateRange } from '../../utils/date';
+import {
+    BrowserRouter,
+    Link,
+  } from 'react-router-dom';
 
 
 
 const ProjectList: React.FC = () => {
     return (
-        <div
-            data-testid="project-list"
-        >
-            <Title title={"People's Projects"} />
-            {
-                mockProjects.map( (mockProject, i) => 
-                    <ProjectCard 
-                        key={i}
-                        name={mockProject.name}
-                        authorName={shortFullName(mockProject.author.firstName, mockProject.author.lastName)}
-                        dateRange={projectDateRange(mockProject.startDate, mockProject.endDate)}
-                    />
-                )
-            }
-        </div>
+        <BrowserRouter>
+            <div
+                data-testid="project-list"
+            >
+                <Title title={"People's Projects"} />
+                {
+                    mockProjects.map( (mockProject, i) => 
+                        <Link 
+                            to={mockProject.id}
+                            key={i}
+                        >
+                            <ProjectCard 
+                                name={mockProject.name}
+                                authorName={shortFullName(mockProject.author.firstName, mockProject.author.lastName)}
+                                dateRange={projectDateRange(mockProject.startDate, mockProject.endDate)}
+                            />
+                        </Link>
+                    )
+                }
+            </div>
+        </BrowserRouter>
     )
 }
 
--- src/utils/mockProjectsData.ts
+++ src/utils/mockProjectsData.ts
@@ -1,5 +1,6 @@
 export const mockProjects = [
     {
+        id: "dfpoigjerpoig",
         name: "Get 6 pack",
         startDate: new Date(2020, 9, 1),
         endDate: new Date(2020, 11, 12),
@@ -54,14 +55,15 @@ export const mockProjects = [
         ]
     },
     {
-        name: "Get 6 pack",
+        id: "agphgwrsdoi",
+        name: "Get a first copywriting gig",
         startDate: new Date(2020, 9, 1),
         endDate: new Date(2020, 11, 12),
         author: 
         // !userid will come here instead of an object
             {
-                firstName: "Takahiro",
-                lastName: "Hasegawa",
+                firstName: "Alisa",
+                lastName: "Kawase",
             },
         accomplishmentStatement: "I'm feeling so great and looking amazing! This is the best shape I have ever got in my life. My instagram would be on fire!!",
         requiredResults: []

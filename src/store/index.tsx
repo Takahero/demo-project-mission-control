f87c7755f3e1db7bab1718410ed625887972be28
Takahiro Hasegawa <23368890+Takahero@users.noreply.github.com>
2020-10-08 21:05:22 +0300

    Ignore firebase actions for serializableCheck

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,18 +1,66 @@
-import { configureStore, combineReducers } from "@reduxjs/toolkit"
-import { firebaseReducer, FirebaseReducer } from "react-redux-firebase"
+import {
+	configureStore,
+	combineReducers,
+	getDefaultMiddleware,
+} from "@reduxjs/toolkit"
+import {
+	firebaseReducer,
+	FirebaseReducer,
+	getFirebase,
+	actionTypes as rrfActionTypes,
+} from "react-redux-firebase"
+import firebase from "firebase/app"
+import { constants as rfConstants } from "redux-firestore"
 
 interface FirebaseState {
-  firebase: FirebaseReducer.Reducer
+	firebase: FirebaseReducer.Reducer
 }
 
 const firebaseRootReducer = combineReducers<FirebaseState>({
-  firebase: firebaseReducer,
+	firebase: firebaseReducer,
 })
+
+const extraArgument = {
+	getFirebase,
+}
+
+const middleware = [
+	...getDefaultMiddleware({
+		serializableCheck: {
+			ignoredActions: [
+				// just ignore every redux-firebase and react-redux-firebase action type
+				...Object.keys(rfConstants.actionTypes).map(
+					(type) => `${rfConstants.actionsPrefix}/${type}`
+				),
+				...Object.keys(rrfActionTypes).map(
+					(type) => `@@reactReduxFirebase/${type}`
+				),
+			],
+			ignoredPaths: ["firebase", "firestore"],
+		},
+		thunk: {
+			extraArgument,
+		},
+	}),
+]
 
 const store = configureStore({
-  reducer: firebaseRootReducer,
+	reducer: firebaseRootReducer,
+	middleware,
 })
 
-export type RootState = ReturnType<typeof store.getState>
+const rrfConfig = {
+	userProfile: "users",
+	// useFirestoreForProfile: true // Firestore for Profile instead of Realtime DB
+}
 
+const rrfProps = {
+	firebase,
+	config: rrfConfig,
+	dispatch: store.dispatch,
+	// createFirestoreInstance // <- needed if using firestore
+}
+
+export type RootState = ReturnType<typeof store.getState>
+export { rrfProps }
 export default store

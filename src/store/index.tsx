66ad318a6797921b101aaa7e03c3a0da3457f72d
Takahiro Hasegawa <23368890+Takahero@users.noreply.github.com>
2020-10-08 18:51:18 +0300

    Configure firebase to store

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import userReducer from './user'
+import { firebaseReducer } from 'react-redux-firebase'
 
 const store = configureStore({
     reducer: {
         user: userReducer,
+        firebase: firebaseReducer,
     }
 })
 

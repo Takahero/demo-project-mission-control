a74991a6c5f4bee21c1bbc30aa1c3c56dd495e60
Takahiro Hasegawa <23368890+Takahero@users.noreply.github.com>
2020-10-09 20:07:26 +0300

    Get projects collection from firestore and listen

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,28 @@ import React from "react"
 import Dashboard from "./pages/Dashboard"
 import Auth from "./pages/Auth"
 import "./App.css"
-import { Route, Redirect } from "react-router-dom"
-import { isLoaded } from "react-redux-firebase"
+import {
+	Route,
+	Redirect
+} from "react-router-dom"
+import {
+	isLoaded,
+	useFirestore
+} from "react-redux-firebase"
 import { useSelector } from "react-redux"
 import { RootState } from "./store"
+
 
 const App: React.FC = () => {
   const auth = useSelector((state: RootState) => state.firebase.auth)
   const isInitializing = useSelector((state: RootState) => state.firebase.isInitializing)
+  const firestore = useFirestore()
+
+	firestore.get({ collection: "projects" })
+	firestore.setListeners([
+		{ collection: 'projects' },
+	])
+
 	if (!isLoaded(auth) || isInitializing) {
 		return <div data-testid="app">loading...</div>
 	}

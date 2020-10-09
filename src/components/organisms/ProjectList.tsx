a7c4f533a9ba7997ee714ca1efe5c9dcccd11c2c
Takahiro Hasegawa <23368890+Takahero@users.noreply.github.com>
2020-10-09 04:28:55 +0300

    Link to create project when authed

--- src/components/organisms/ProjectList.tsx
+++ src/components/organisms/ProjectList.tsx
@@ -1,33 +1,47 @@
-import React from 'react'
-import Title from '../atoms/Texts/Title'
-import ProjectCard from '../molecules/ProjectCard'
-import { mockProjects } from '../../utils/mockProjectsData'
-import { shortFullName } from '../../utils/name'
-import { projectDateRange } from '../../utils/date'
-import { Link } from 'react-router-dom'
+import React from "react"
+import Title from "../atoms/Texts/Title"
+import ProjectCard from "../molecules/ProjectCard"
+import { mockProjects } from "../../utils/mockProjectsData"
+import { shortFullName } from "../../utils/name"
+import { projectDateRange } from "../../utils/date"
+import { Link } from "react-router-dom"
+import { isEmpty } from "react-redux-firebase"
+import { useSelector } from "react-redux"
+import { RootState } from "../../store"
+import { Redirect } from "react-router-dom"
+import NavButton from "../atoms/Buttons/NavButton"
 
 const ProjectList: React.FC = () => {
-    return (
-        <div
-            data-testid="project-list"
-        >
-            <Title title={"People's Projects"} />
-            {
-                mockProjects.map( (mockProject, i) => 
-                    <Link 
-                        to={`/project/${mockProject.id}`}
-                        key={i}
-                    >
-                        <ProjectCard 
-                            name={mockProject.name}
-                            authorName={shortFullName(mockProject.author.firstName, mockProject.author.lastName)}
-                            dateRange={projectDateRange(mockProject.startDate, mockProject.endDate)}
-                        />
-                    </Link>
-                )
-            }
-        </div>
-    )
+	const auth = useSelector((state: RootState) => state.firebase.auth)
+
+	return (
+		<div data-testid="project-list">
+			<Title title={"People's Projects"} />
+
+            { !isEmpty(auth) &&
+				<NavButton
+					text="Create a new project"
+					path="/project/create"
+				/>
+			}
+
+			{mockProjects.map((mockProject, i) => (
+				<Link to={`/project/${mockProject.id}`} key={i}>
+					<ProjectCard
+						name={mockProject.name}
+						authorName={shortFullName(
+							mockProject.author.firstName,
+							mockProject.author.lastName
+						)}
+						dateRange={projectDateRange(
+							mockProject.startDate,
+							mockProject.endDate
+						)}
+					/>
+				</Link>
+			))}
+		</div>
+	)
 }
 
 export default ProjectList

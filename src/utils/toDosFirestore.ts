002e5c987fd4311a9d1e098efb8d8b0f37940f17
Takahiro Hasegawa <23368890+Takahero@users.noreply.github.com>
2020-10-13 18:34:19 +0300

    Refactor toDosFirestore

--- src/utils/toDosFirestore.ts
+++ src/utils/toDosFirestore.ts
@@ -5,7 +5,7 @@ const sortToDosByDate = (toDos: any) => {
     if (toDos) {
         return toDos.slice().sort((a:any, b:any) => {
             return a.createdAt - b.createdAt
-        }) 
+        })
     }
     return null
 }
@@ -29,8 +29,8 @@ const addToDo = (firestore: any, projectId: string, requiredResultId: string, na
     }).catch((e: Error) => console.error(e))
 }
 
-const updateToDo = async (firestore: any, projectId: string, requiredResultId: string, name: string, toDo: any) => {
-    await toDo && firestore.update({
+const deleteToDo = (firestore: any, projectId: string, requiredResultId: string, toDo: any) => {
+    toDo && firestore.update({
         collection: 'projects',
         doc: projectId,
         subcollections: [{
@@ -41,64 +41,51 @@ const updateToDo = async (firestore: any, projectId: string, requiredResultId: s
     }, {
         toDos: firestore.FieldValue.arrayRemove(toDo)
     }).catch((e: Error) => console.error(e))
-    firestore.update({
-        collection: 'projects',
-        doc: projectId,
-        subcollections: [{
-            collection: 'requiredResults',
-            doc: requiredResultId
-        }],
-        storeAs: 'requiredResults'
-    }, {
-        toDos: firestore.FieldValue.arrayUnion({
-            ...toDo,
-            name
-        })
-    }).catch((e: Error) => console.error(e))
 }
 
-// const completeToDo = async (firestore: any, projectId: string, requiredResultId: string, toDo: any) => {
-//     await toDo && firestore.update({
-//         collection: 'projects',
-//         doc: projectId,
-//         subcollections: [{
-//             collection: 'requiredResults',
-//             doc: requiredResultId
-//         }],
-//         storeAs: 'requiredResults'
-//     }, {
-//         toDos: firestore.FieldValue.arrayRemove(toDo)
-//     }).catch((e: Error) => console.error(e))
-//     firestore.update({
-//         collection: 'projects',
-//         doc: projectId,
-//         subcollections: [{
-//             collection: 'requiredResults',
-//             doc: requiredResultId
-//         }],
-//         storeAs: 'requiredResults'
-//     }, {
-//         toDos: firestore.FieldValue.arrayUnion({
-//             ...toDo,
-//             completed: !toDo.completed,
-//         })
-//     }).catch((e: Error) => console.error(e))
-// }
+const updateToDo = async (firestore: any, projectId: string, requiredResultId: string, name: string, toDo: any) => {
+    if (toDo) {
+        await deleteToDo(firestore, projectId, requiredResultId, toDo)
+        firestore.update({
+            collection: 'projects',
+            doc: projectId,
+            subcollections: [{
+                collection: 'requiredResults',
+                doc: requiredResultId
+            }],
+            storeAs: 'requiredResults'
+        }, {
+            toDos: firestore.FieldValue.arrayUnion({
+                ...toDo,
+                name
+            })
+        }).catch((e: Error) => console.error(e))
+    }
+}
 
-// const deleteRequiredResult = (firestore: any, projectId: string, requiredResultId:string) => {
-//     firestore.delete({
-//         collection: 'projects',
-//         doc: projectId,
-//         subcollections: [{
-//             collection: 'requiredResults',
-//             doc: requiredResultId
-//         }],
-//         storeAs: 'requiredResults'
-//     }).catch((e: Error) => console.error(e))
-// }
+const completeToDo = async (firestore: any, projectId: string, requiredResultId: string, toDo: any) => {
+    if (toDo) {
+        await deleteToDo(firestore, projectId, requiredResultId, toDo)
+        firestore.update({
+            collection: 'projects',
+            doc: projectId,
+            subcollections: [{
+                collection: 'requiredResults',
+                doc: requiredResultId
+            }],
+            storeAs: 'requiredResults'
+        }, {
+            toDos: firestore.FieldValue.arrayUnion({
+                ...toDo,
+                completed: !toDo.completed,
+            })
+        }).catch((e: Error) => console.error(e))
+    }
+}
 
 export {
     sortToDosByDate,
+    deleteToDo,
     addToDo,
     updateToDo,
 }
